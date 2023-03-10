049895cb186014f6a56fbf4d6ff483a5bb9f870b
HauZaakMing <houzeming123@163.com>
2023-03-10 09:31:26 +0800

    2023/3/9

--- com/leetcode/leetcode_739.java
+++ com/leetcode/leetcode_739.java
@@ -1,4 +1,26 @@
 package com.leetcode;
 
+import com.sun.nio.sctp.AssociationChangeNotification;
+
+import java.util.Stack;
+
 public class leetcode_739 {
+
+    public int[] dailyTemperatures(int[] temperatures) {
+        int []ans = new int[temperatures.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < temperatures.length; i++) {
+            if(stack.isEmpty()){
+                stack.add(i);
+            }else{
+                while(!stack.isEmpty()&&temperatures[i]>temperatures[stack.peek()]){
+                    int pos = stack.pop();
+                    ans[pos] = i-pos;
+                }
+                stack.add(i);
+            }
+
+        }
+        return ans;
+    }
 }
