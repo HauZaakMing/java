1073bcb0913b509d7a4ca77c7c3c8d13fc994690
HauZaakMing <houzeming123@163.com>
2023-03-14 21:50:34 +0800

    2023/3/14

--- com/leetcode/leetcode_416.java
+++ com/leetcode/leetcode_416.java
@@ -59,4 +59,37 @@ public class leetcode_416 {
         }
         return false;
     }
+
+    public boolean canPartition(int[] nums) {
+        if(nums.length<2){
+            return false;
+        }
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum+= nums[i];
+        }
+        if(sum%2==1){
+            return false;
+        }
+        int target = sum>>1;
+        boolean dp[][] = new boolean[nums.length][target+1];
+        for (int i = 0; i < nums.length; i++) {
+            dp[i][0] =true;
+        }
+        if(nums[0]<=target){
+            dp[0][nums[0]] = true;
+        }
+
+        for (int i = 1; i < nums.length ; i++) {
+            for (int j = 1; j < target+1 ; j++) {
+                if(j>=nums[i]){
+                    dp[i][j] = dp[i-1][j-nums[i]]|dp[i-1][j];
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+
+            }
+        }
+        return dp[nums.length-1][target];
+    }
 }
