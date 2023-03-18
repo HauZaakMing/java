1f873e3a902fb8e53b2f2cf94a841d990df49cd5
HauZaakMing <houzeming123@163.com>
2023-03-18 17:02:58 +0800

    2023/3/18

--- com/leetcode/leetcode_200.java
+++ com/leetcode/leetcode_200.java
@@ -1,20 +1,56 @@
 package com.leetcode;
 
-import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class leetcode_200 {
+    public static boolean visited[][];
+    public static final int[][] dirs = { {-1,0},{1,0},{0,-1},{0,1} };
     public int numIslands(char[][] grid) {
         //自节点 岛的根节点
-        HashMap<Node,Node> map = new HashMap<>();
-        int length = grid.length;
-        for (int i = 0; i < length; i++) {
-            for (int j = 0; j < length; j++) {
-                Node n = new Node(i,j);
-                map.put(n,n);
+//        HashMap<Node,Node> map = new HashMap<>();
+        //BFS
+        int length1 = grid.length;
+        int length2 = grid[0].length;
+        visited = new boolean[length1][length2];
+        int count = 0;
+        for (int i = 0; i < length1; i++) {
+            for (int j = 0; j < length2; j++) {
+                if(!visited[i][j]&&grid[i][j]=='1'){
+                    count++;
+                    bfs(grid,i,j);
+                }
             }
         }
-        return 0;
+        return count;
 
+    }
+
+    private void bfs(char[][] grid, int i, int j) {
+
+        Node n = new Node(i,j);
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(n);
+        visited[i][j] = true;
+        while(!queue.isEmpty()){
+
+            Node temp = queue.poll();
+            int x = temp.x;
+            int y = temp.y;
+
+            for (int k = 0; k < 4; k++) {
+                int nx = x+dirs[k][0];
+                int ny = y+dirs[k][1];
+                if(nx<0||ny<0||nx>=grid.length||ny>=grid[0].length){
+                    continue;
+                }
+                if(!visited[nx][ny]&&grid[nx][ny]=='1'){
+                    temp = new Node(nx,ny);
+                    queue.add(temp);
+                    visited[nx][ny] = true;
+                }
+            }
+        }
     }
 
     class Node{
@@ -25,6 +61,11 @@ public class leetcode_200 {
             this.x = x;
             this.y = y;
         }
+    }
+
+    public static void main(String[] args) {
+        leetcode_200 demo = new leetcode_200();
+        demo.numIslands(new char[][]{{'1','1','1','1','0'},{'1','1','0','1','0'},{'1','1','0','0','0'},{'0','0','0','0','0'}});
     }
 
 }
