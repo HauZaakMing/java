81f9ba0474262dc245d08da57be9e6322d818b6f
HauZaakMing <houzeming123@163.com>
2023-03-19 10:10:02 +0800

    2023/3/19

--- com/leetcode/leetcode_42.java
+++ com/leetcode/leetcode_42.java
@@ -36,7 +36,7 @@ public class leetcode_42 {
 
 
     //要是用双指针来减少动态规划的空间消耗
-    public int trap(int[] height) {
+    public int trap2(int[] height) {
         int leftMax = 0;
         int rightMax = 1;
         for (int i = 1; i < height.length; i++) {
@@ -68,6 +68,68 @@ public class leetcode_42 {
             if(height[i]>height[leftMax]){
                 leftMax = i;
             }
+        }
+        return ans;
+    }
+
+    public int trap3(int[] height) {
+        int lMax = 0;
+        int rMax = 0;
+        int left = 1;
+        int right = height.length-2;
+        int ans = 0;
+        for (int i = 1; i < height.length-1 ; i++) {
+            if(height[left-1]>lMax){
+                lMax = height[left-1];
+            }
+            if(height[right+1]>rMax){
+                rMax = height[right+1];
+            }
+            if(lMax<rMax){
+                int min = lMax;
+                if(height[left]<min){
+                    ans += min - height[left];
+                }
+                left++;
+            }else{
+                int min = rMax;
+                if(height[right]<min){
+                    ans+=min-height[right];
+                }
+                right--;
+            }
+
+        }
+        return ans;
+    }
+
+    public int trap(int[] height) {
+
+
+        Stack<Integer> stack = new Stack<>();
+        int length = height.length;
+        int current = 0;
+        int ans =0;
+        while(current<length){
+
+            //要是出现了大于等于栈顶
+            while (!stack.isEmpty()&&height[current]>height[stack.peek()]){
+
+
+                int h = height[stack.pop()];
+
+                if(stack.isEmpty()){
+                    break;
+                }
+                int left = height[stack.peek()];
+                int distance = current-stack.peek()-1;
+                int min = Math.min(height[current],left);
+                if(min>h){
+                   ans+=(distance)*(min-h);
+                }
+            }
+            stack.add(current);
+            current++;
         }
         return ans;
     }
